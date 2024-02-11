a091a4ebeeae43a8477924d06d123619554320ee
HeseltineBTutu <heseltinetutu@gmail.com>
2024-02-11 04:32:23 -0800

    added documentation

--- console.py
+++ console.py
@@ -14,7 +14,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_quit(self, arg):
-        """Exit the program."""
+        """Quit command to exit the program"""
         return True
 
     def emptyline(self):
