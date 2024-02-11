7d0e1193a6d38c2d6f218971ebad108e97a309aa
HeseltineBTutu <heseltinetutu@gmail.com>
2024-02-11 00:02:21 -0800

    docs: Add module-level documentation for FileStorage module
    This commit adds a module-level docstring to the FileStorage module, providing
    an overview of its purpose, functionality, and usage. The documentation
    describes the FileStorage class, its role in serializing and deserializing
    instances, and provides usage examples. This documentation will help users
    understand how to use the module effectively.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,37 @@
 import json
 import os
 import importlib
+"""
+FileStorage module provides the FileStorage class, which serializes instances
+to a JSON file and deserializes JSON file to instances.
+
+This module contains the FileStorage class, which facilitates the storage and
+retrieval of objects by serializing them to a JSON file and deserializing JSON
+data back into Python objects. It offers methods for managing objects in a
+persistent manner, allowing data to be saved and reloaded across program
+sessions.
+
+Classes:
+    FileStorage: Class for serializing instances to a JSON file and
+    deserializing JSON file to instances.
+
+Usage:
+    # Example usage of the FileStorage class
+    from models.engine.file_storage import FileStorage
+
+    # Create a FileStorage instance
+    storage = FileStorage()
+
+    # Save objects to the JSON file
+    storage.save()
+
+    # Reload objects from the JSON file
+    storage.reload()
+
+    # Access stored objects
+    objects = storage.all()
+"""
+
 
 class FileStorage:
     """
@@ -30,7 +61,10 @@ class FileStorage:
         """
         Serializes __objects to the JSON file (__file_path).
         """
-        serialized_objects = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
+        serialized_objects = {
+                key: obj.to_dict()
+                for key, obj in FileStorage.__objects.items()
+                }
         with open(FileStorage.__file_path, "w") as f:
             json.dump(serialized_objects, f)
 
@@ -48,7 +82,8 @@ class FileStorage:
                     module = importlib.import_module(module_name)
                     cls = getattr(module, class_name)
 
-                    # Add new objects from the JSON file if they do not already exist
+                    # Add new objects from the JSON file
+                    # if they do not already exist
                     if key not in FileStorage.__objects:
                         obj_instance = cls(**value)
                         FileStorage.__objects[key] = obj_instance
