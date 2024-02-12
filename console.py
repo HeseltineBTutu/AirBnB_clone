9d031d96f5eb9a1a7dabf8420e494bddd98b5d45
HeseltineBTutu <heseltinetutu@gmail.com>
2024-02-12 00:59:53 -0800

    Update command interpreter with 'create' feature for BaseModel instances

--- console.py
+++ console.py
@@ -4,6 +4,8 @@ This module provides a command-line interface
 for interacting with an AirBnB clone.
 """
 import cmd
+from models.base_model import BaseModel
+import json
 
 
 class HBNBCommand(cmd.Cmd):
@@ -21,11 +23,30 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
-    def do_helpi(self, arg):
+    def do_help(self, arg):
         """Get help on commands"""
         if arg:
             super().do_help(arg)
 
+    def do_create(self, arg):
+        """Create a new instance of BaseModel and save it to the JSON file"""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        # Map class names to their corresponding Python class
+        class_mapping = {
+                'BaselModel': BaseModel
+                }
+
+        class_name = arg.split()[0]
+        if class_name not in class_mapping:
+            print("** class doesn't exist **")
+            return
+
+        new_instance = class_mapping[class_name]()
+        new_instance.save()
+        print(new_instance.id)
 
 
 if __name__ == '__main__':
