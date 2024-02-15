feb89cfed0e2b7f016615b001e13f0935c70b3a5
HeseltineBTutu <heseltinetutu@gmail.com>
2024-02-15 05:08:23 -0800

    added state model

---
+++ models/state.py
@@ -0,0 +1,16 @@
+"""
+Module defining the State class.
+
+This module contains the definition of the State class,
+which represents a state in some context.
+"""
+
+
+class State(BaseModel):
+    """
+    Class definition of state.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+    name = ""
