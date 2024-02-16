a13aa1c21bf95d3ffa06e3f05a180f20a3de8afd
HeseltineBTutu <heseltinetutu@gmail.com>
2024-02-16 03:18:32 -0800

    import BaseModel

--- models/review.py
+++ models/review.py
@@ -4,6 +4,9 @@ Module defining the Review class.
 This module contains the definition of the Review class,
 which represents a review associated with a place and a user.
 """
+from .base_model import BaseModel
+
+
 class Review(BaseModel):
     place_id = ""
     user_id = ""
